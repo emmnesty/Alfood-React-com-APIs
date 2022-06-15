8de86eac99435709bce9d77ca2ad2dcacaeaec5e
emmnesty <elyjhonatan@hotmail.com>
2022-06-15 08:54:45 -0300

    Colocando o Delete e campo de busca

--- alfood-main/src/componentes/ListaRestaurantes/index.tsx
+++ alfood-main/src/componentes/ListaRestaurantes/index.tsx
@@ -51,7 +51,6 @@
 
 // export default ListaRestaurantes;
 
-
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { IPaginacao } from '../../interfaces/IPaginacao';
@@ -60,38 +59,54 @@ import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
 
 const ListaRestaurantes = () => {
-
-  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
-  const [proximaPagina, setProximaPagina] = useState('')
-  const [paginaAnterior, setPaginaAnterior] = useState('')
+  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
+  const [proximaPagina, setProximaPagina] = useState('');
+  const [paginaAnterior, setPaginaAnterior] = useState('');
 
   const carregarDados = (url: string) => {
-    axios.get<IPaginacao<IRestaurante>>(url)
-      .then(resposta => {
-        setRestaurantes(resposta.data.results)
-        setProximaPagina(resposta.data.next)
-        setPaginaAnterior(resposta.data.previous)
+    axios
+      .get<IPaginacao<IRestaurante>>(url)
+      .then((resposta) => {
+        setRestaurantes(resposta.data.results);
+        setProximaPagina(resposta.data.next);
+        setPaginaAnterior(resposta.data.previous);
       })
-      .catch(erro => {
-        console.log(erro)
-      })
-  }
+      .catch((erro) => {
+        console.log(erro);
+      });
+  };
 
   useEffect(() => {
     // obter restaurantes
-    carregarDados('http://localhost:8000/api/v1/restaurantes/')
-  }, [])
+    carregarDados('http://localhost:8000/api/v1/restaurantes/');
+  }, []);
 
-  return (<section className={style.ListaRestaurantes}>
-    <h1>Os restaurantes mais <em>bacanas</em>!</h1>
-    {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
-    {<button onClick={() => carregarDados(paginaAnterior)} disabled={!paginaAnterior}>
-      Página Anterior
-    </button>}
-    {<button onClick={() => carregarDados(proximaPagina)} disabled={!proximaPagina}>
-      Próxima página
-    </button>}
-  </section>)
-}
+  return (
+    <section className={style.ListaRestaurantes}>
+      <h1>
+        Os restaurantes mais <em>bacanas</em>!
+      </h1>
+      {restaurantes?.map((item) => (
+        <Restaurante restaurante={item} key={item.id} />
+      ))}
+      {
+        <button
+          onClick={() => carregarDados(paginaAnterior)}
+          disabled={!paginaAnterior}
+        >
+          Página Anterior
+        </button>
+      }
+      {
+        <button
+          onClick={() => carregarDados(proximaPagina)}
+          disabled={!proximaPagina}
+        >
+          Próxima página
+        </button>
+      }
+    </section>
+  );
+};
 
-export default ListaRestaurantes
+export default ListaRestaurantes;
